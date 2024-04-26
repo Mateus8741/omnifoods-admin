e35efe988f9ebccef7a4a9e284e17905b79c603f
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-26 11:51:40 -0300

    Add colors module to theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  bg: '#151518',
+
+  gray: {
+    subtitle: '#7F7F81',
+    light: '#434345',
+    1000: '#26262D',
+  },
+
+  green: {
+    success: '#00C853',
+  },
+
+  red: {
+    line: '#B70F0F',
+  },
+};
