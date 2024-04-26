423dc00f83a2ad01f3164ebbece646646da1df8d
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-26 16:30:23 -0300

    :boom: fix: boder color and shadow

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -41,7 +41,7 @@ export function Sidebar() {
   ]
 
   return (
-    <div className="flex flex-col gap-3 w-[16rem] border-r border-r-gray-1000/25 min-h-screen p-4 shadow-lg shadow-gray-1000/30">
+    <div className="flex flex-col gap-3 w-[16rem] border-r border-r-white/25 min-h-screen p-4 shadow-lg shadow-white/30">
       <div>
         <UserItem />
       </div>
