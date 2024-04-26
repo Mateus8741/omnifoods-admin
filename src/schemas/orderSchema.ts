1439ebaff1dbc47ba26515ef47ef7f515543fcaa
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-26 18:30:52 -0300

    Add orderSchema.ts to define the Order interface

---
+++ src/schemas/orderSchema.ts
@@ -0,0 +1,15 @@
+interface ProductOrder {
+  id: string
+  productName: string
+  productPrice: number
+  quantity: number
+  orderId: string
+}
+
+export interface Order {
+  id: string
+  tableNumber: number
+  changeToOrder: string
+  createdAt: string
+  productOrders: ProductOrder[]
+}
