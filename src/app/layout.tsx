6b3d15acc0a25b21531d89b2d6b01e89269f8a2b
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-26 16:26:08 -0300

    :boom: fix: main space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} grid grid-cols-[16rem,1fr] bg-bg`}>
         <Sidebar />
-        {children}
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {children}
+        </main>
       </body>
     </html>
   )
