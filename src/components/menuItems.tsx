1637fc92deb69c276060fe0ef68343051f4ceada
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-05-09 15:09:34 -0300

    :sparkles: feat: Add hover on current page

--- src/components/menuItems.tsx
+++ src/components/menuItems.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type MenuItem = {
   group: {
@@ -12,6 +13,8 @@ type MenuItem = {
 }
 
 export function MenuItems({ group }: MenuItem) {
+  const pathName = usePathname()
+
   return (
     <div className="flex flex-col gap-2">
       <h2 className="text-sm font-bold text-gray-1000">{group.group}</h2>
@@ -20,7 +23,9 @@ export function MenuItems({ group }: MenuItem) {
           <Link
             key={index}
             href={item.link}
-            className="flex gap-2 p-2 rounded-md items-center text-gray-subtitle hover:bg-gray-1000/10"
+            className={`flex gap-2 p-2 rounded-md items-center text-gray-subtitle hover:bg-gray-1000/10 ${
+              pathName === item.link && 'bg-gray-1000/10'
+            }`}
           >
             {item.icon}
             {item.text}
