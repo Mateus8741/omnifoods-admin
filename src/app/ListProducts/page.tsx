8a196a29fa0f4320e16a75bc13a6e57473eeac28
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-05-09 18:04:13 -0300

    feat: Add loading spinner while products are being fetched

--- src/app/ListProducts/page.tsx
+++ src/app/ListProducts/page.tsx
@@ -2,16 +2,24 @@
 
 import { useListProducts } from '@/api/useCases/useListProducts'
 import { ProductCard } from '@/components/productCard'
+import { LoaderCircle } from 'lucide-react'
 
 export default function ListProducts() {
   const { data, error, isLoading } = useListProducts()
+
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <LoaderCircle className="animate-spin h-12 w-12" />
+      </div>
+    )
+  }
 
   return (
     <div className="flex flex-col justify-center">
       <h2 className="mb-12 text-2xl">Produtos listados</h2>
 
       <div className="">
-        {isLoading && <p>Carregando...</p>}
         {error && <p>Erro ao carregar produtos</p>}
 
         {data?.data?.map((product) => (
