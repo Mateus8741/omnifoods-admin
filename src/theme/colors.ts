b8c9768b37ae6522f962bfcb660d6b49a424af62
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2024-05-13 17:52:52 -0300

    refactor: Update colors.ts with additional gray and green shades
    - Added new shades of gray and green to the colors object in colors.ts
    - Gray shades include background, icons, subtitle, and light
    - Green shades include success and completed
    - Updated existing red shades with new shades for line, dark, danger, canceled, and light
    - Yellow shades include pending
    
    #changelog
    - Added new gray and green shades
    - Updated existing red shades
    - Added yellow shades

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -5,6 +5,8 @@ export const colors = {
   bg: '#121214',
 
   gray: {
+    bg: '#F0EEEE',
+    icons: '#84919A',
     subtitle: '#7F7F81',
     light: '#434345',
     1000: '#26262D',
@@ -12,10 +14,18 @@ export const colors = {
 
   green: {
     success: '#00C853',
+    completed: '#72E768',
+  },
+
+  yellow: {
+    pending: '#E7D268',
   },
 
   red: {
     line: '#B70F0F',
+    dark: '#541818',
     danger: '#FF3D00',
+    canceled: '#E76868',
+    light: '#F2E2E2',
   },
 }
