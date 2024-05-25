ffdb8775e77dd5e41642b1b05e741f324d9861f8
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-05-25 19:34:02 -0300

    refactor: Update page component to display orders by status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,51 +6,6 @@ import { Status } from '@/components/status'
 import { Order } from '@/schemas/orderSchema'
 
 export default function Home() {
-  // const { data } = useListAllOrders()
-
-  // const pendingOrders = data?.data.filter((order) => order.status === 'PENDING')
-  // const canceledOrders = data?.data.filter(
-  //   (order) => order.status === 'CANCELLED',
-  // )
-  // const completedOrders = data?.data.filter(
-  //   (order) => order.status === 'COMPLETED',
-  // )
-
-  // return (
-  //   <div className="w-full h-full p-4">
-  //     <h1 className="text-2xl font-bold text-gray-1000">Pedidos</h1>
-
-  //     <div className="grid grid-cols-3 gap-4 mt-4">
-  //       <div className="flex flex-col px-2 py-5 bg-gray-bg rounded-3xl">
-  //         <Status statusType="pending" />
-
-  //         {pendingOrders &&
-  //           pendingOrders.map((order) => (
-  //             <CardOrders key={order.tableNumber} {...order} />
-  //           ))}
-  //       </div>
-
-  //       <div className="flex flex-col px-2 py-5 bg-gray-bg rounded-3xl">
-  //         <Status statusType="canceled" />
-
-  //         {canceledOrders &&
-  //           canceledOrders.map((order) => (
-  //             <CardOrders key={order.tableNumber} {...order} />
-  //           ))}
-  //       </div>
-
-  //       <div className="flex flex-col px-2 py-5 bg-gray-bg rounded-3xl">
-  //         <Status statusType="completed" />
-
-  //         {completedOrders &&
-  //           completedOrders.map((order) => (
-  //             <CardOrders key={order.tableNumber} {...order} />
-  //           ))}
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
-
   const { data } = useListAllOrders()
 
   const ordersByStatus = data?.data.reduce(
@@ -76,7 +31,7 @@ export default function Home() {
         {Object.entries(ordersByStatus || {}).map(([status, orders]) => (
           <div
             key={status}
-            className="flex flex-col px-2 py-5 bg-gray-bg rounded-3xl"
+            className="flex flex-col px-3 py-5 bg-gray-bg rounded-3xl"
           >
             <Status
               statusType={
