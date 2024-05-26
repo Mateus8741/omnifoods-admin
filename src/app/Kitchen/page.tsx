3b15ebd9ab4f26b0e1c19e5c6217b7ed69807dd6
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-05-26 13:08:01 -0300

    refactor: Update page component to display orders by status

--- src/app/Kitchen/page.tsx
+++ src/app/Kitchen/page.tsx
@@ -3,26 +3,47 @@
 import { useListAllOrders } from '@/api/useCases/useListAllOrders'
 import { CardOrders } from '@/components/cardOrders'
 import { Status } from '@/components/status'
+import { Order } from '@/schemas/orderSchema'
 
-export default function Billing() {
+export default function Home() {
   const { data } = useListAllOrders()
 
-  const pendingOrders = data?.data.filter(
-    (order) => order.status === 'PREPARING',
+  const ordersByStatus = data?.data.reduce(
+    (acc: { [key: string]: Order[] }, order) => {
+      if (order.status === 'PREPARING' || order.status === 'COMPLETED') {
+        if (!acc[order.status]) {
+          acc[order.status] = []
+        }
+        acc[order.status].push(order)
+      }
+      return acc
+    },
+    {
+      PREPARING: [],
+      COMPLETED: [],
+    },
   )
 
   return (
-    <div className="">
-      <h1 className="text-2xl font-bold text-gray-1000">Pedidos</h1>
+    <div className="w-full h-full p-4">
+      <h1 className="text-2xl font-bold text-gray-1000">Cozinha</h1>
 
-      <div className="grid grid-cols-3 gap-4 mt-4">
-        <div className="flex flex-col px-2 py-5 bg-gray-bg rounded-3xl">
-          <Status statusType="preparing" />
-          {pendingOrders &&
-            pendingOrders.map((order) => (
-              <CardOrders key={order.id} {...order} />
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        {Object.entries(ordersByStatus || {}).map(([status, orders]) => (
+          <div
+            key={status}
+            className="flex flex-col px-3 py-5 bg-gray-bg rounded-3xl"
+          >
+            <Status
+              statusType={
+                status.toLowerCase() as 'pending' | 'preparing' | 'completed'
+              }
+            />
+            {(orders as Order[]).map((order: Order) => (
+              <CardOrders key={order.tableNumber} {...order} />
             ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   )
