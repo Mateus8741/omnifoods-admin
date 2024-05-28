220612c2c97a331584889216dccb7810fb8a16d3
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-05-28 14:42:24 -0300

    refactor: Update AddProducts page UI and styling

--- src/app/AddProducts/page.tsx
+++ src/app/AddProducts/page.tsx
@@ -50,23 +50,33 @@ export default function CreateProduct() {
   }
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <form className="flex h-full flex-col justify-center gap-4">
-        <div className="w-1/2">
+    <div className="w-full h-full items-center justify-center">
+      <h2 className="mb-4 text-2xl">Produtos</h2>
+
+      <form className="flex flex-col justify-center gap-4 p-4 w-[32rem]">
+        <div className="flex justify-between gap-x-4">
           <FormTextInput control={control} name="title" label="Categoria" />
+
+          <FormTextInput control={control} name="name" label="Nome" />
         </div>
 
-        <div className="flex items-center gap-6">
-          <div>
-            <h2 className="mb-4 text-2xl">Produtos</h2>
+        <div className="flex items-center justify-between gap-x-4">
+          <div className="bg-black h-1 w-full" />
+          <h2 className="text-2xl">Detalhes</h2>
+          <div className="bg-black h-1 w-full" />
+        </div>
 
-            <FormTextInput control={control} name="name" label="Nome" />
-            <FormTextInput control={control} name="price" label="Preço" />
-            <FormTextInput
-              control={control}
-              name="ingredients"
-              label="Ingredientes"
-            />
+        <div className="">
+          <div>
+            <div className="flex gap-5">
+              <FormTextInput control={control} name="price" label="Preço" />
+              <FormTextInput
+                control={control}
+                name="ingredients"
+                label="Ingredientes"
+              />
+            </div>
+
             <FormTextArea
               control={control}
               name="description"
@@ -74,61 +84,67 @@ export default function CreateProduct() {
               maxLength={200}
             />
           </div>
+        </div>
 
-          <div className="flex gap-5">
-            {thumbnail ? (
-              <Image
-                src={thumbnail}
-                alt="Thumbnail"
-                width={32}
-                height={32}
-                className="w-32 h-32 rounded-md"
-                quality={100}
+        <div className="flex items-center justify-between gap-x-4">
+          <div className="bg-black h-1 w-full" />
+          <h2 className="text-2xl">Imagens</h2>
+          <div className="bg-black h-1 w-full" />
+        </div>
+
+        <div className="flex items-center justify-center gap-5">
+          {thumbnail ? (
+            <Image
+              src={thumbnail}
+              alt="Thumbnail"
+              width={32}
+              height={32}
+              className="w-32 h-32 rounded-md"
+              quality={100}
+            />
+          ) : (
+            <div className="flex flex-col items-center justify-center text-white">
+              <UploadButton
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  console.log('Files: ', res)
+                  setThumbnail(res[0].url)
+                }}
+                onUploadError={(error: Error) => {
+                  alert(`ERROR! ${error.message}`)
+                }}
+                className="w-32 h-32 bg-gray-light rounded-md"
               />
-            ) : (
-              <div className="flex flex-col items-center justify-center">
-                <UploadButton
-                  endpoint="imageUploader"
-                  onClientUploadComplete={(res) => {
-                    console.log('Files: ', res)
-                    setThumbnail(res[0].url)
-                  }}
-                  onUploadError={(error: Error) => {
-                    alert(`ERROR! ${error.message}`)
-                  }}
-                  className="w-32 h-32 bg-gray-light rounded-md"
-                />
 
-                <p>Thumbnail</p>
-              </div>
-            )}
+              <p>Thumbnail</p>
+            </div>
+          )}
 
-            {cover ? (
-              <Image
-                src={cover}
-                alt="Cover"
-                width={32}
-                height={32}
-                className="w-32 h-32 rounded-md"
+          {cover ? (
+            <Image
+              src={cover}
+              alt="Cover"
+              width={32}
+              height={32}
+              className="w-32 h-32 rounded-md"
+            />
+          ) : (
+            <div className="flex flex-col items-center justify-center text-white">
+              <UploadButton
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  console.log('Files: ', res)
+                  setCover(res[0].url)
+                }}
+                onUploadError={(error: Error) => {
+                  alert(`ERROR! ${error.message}`)
+                }}
+                className="w-32 h-32 bg-gray-light rounded-md"
               />
-            ) : (
-              <div className="flex flex-col items-center justify-center">
-                <UploadButton
-                  endpoint="imageUploader"
-                  onClientUploadComplete={(res) => {
-                    console.log('Files: ', res)
-                    setCover(res[0].url)
-                  }}
-                  onUploadError={(error: Error) => {
-                    alert(`ERROR! ${error.message}`)
-                  }}
-                  className="w-32 h-32 bg-gray-light rounded-md"
-                />
 
-                <p>Cover</p>
-              </div>
-            )}
-          </div>
+              <p>Cover</p>
+            </div>
+          )}
         </div>
 
         <CustomButton
