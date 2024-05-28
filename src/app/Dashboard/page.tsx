65fd33f0fdd8500c0d91f092549cc17c64c4e09c
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-05-28 16:31:15 -0300

    refactor: Create Dashboard page component

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="w-full h-full p-4">
+      <h1 className="text-2xl font-bold text-gray-1000">Dashboard</h1>
+
+      <div></div>
+    </div>
+  )
+}
